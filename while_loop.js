e2d1eb8e2cc25093b9ae07111aab11bcd4c4ab61
Jonathan Kaz <contact@redstarfilmsnc.com>
2017-01-28 14:13:50 -0500

    loops

---
+++ while_loop.js
@@ -0,0 +1,44 @@
+var chair = 10;
+ while (chair > 5) {
+ 	console.log('cushion');
+ 	chair = chair - 2;
+ }
+
+
+var sandal = 1;
+ while (sandal < 7) {
+ 	console.log('Christ Kickers');
+ 	sandal = sandal * 2;
+ }
+
+
+var wiggles = 0;
+var squiggles = 0;
+while (wiggles < 3) {
+	squiggles = squiggles +1;
+	if (squiggles > 4) {
+		wiggles = 6;
+	}
+	console.log('merry go f yourself!');
+}
+
+var i = 0;
+while (i < 99) {
+
+    // This block will be executed 100 times
+    console.log('Currently at ' + i);
+
+    i++; // increment i
+
+}
+
+//while loop increments of 3 up to 99
+
+var i = 0;
+while (i < 34) {
+	
+    console.log('number is ' + i*3);
+
+    i++; // increment i
+
+}
